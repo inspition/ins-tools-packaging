1bd13d1546dc20720593c681964d3e1c00570a94
林春通 <linchuntong@dataexa.com>
2022-07-26 18:26:08 +0800

    ✨ feat: 整合新方法ts转换

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,35 @@
 declare namespace insToolsPackaging {
-  /**
-   * 生成数字范围内的随机数
-   * @param min 最小数字
-   * @param max 最大数字
-   * @returns number类型
-   */
   export function random(min: number, max: number): number
+
+  export function getValue(fn: () => any, defaultValue?: any): void
+
+  export function chainAccess(result: AnyObject, path: string): void
+
+  export function joinDebounce(): void
+
+  export function genrateParallels(apis: Function[]): void
+
+  export function $thenBack(res: AnyObject): void
+
+  export function $catchBack(errPrefix: string): void
+
+  export function $confirmReq(tip: string, thenBack: () => null): void
+
+  export function apiReq(api: AxiosInstance): void
+
+  export function $downloadFile(res: AnyObject): void
+
+  export function itemFiledsMap(fieldsMap: AnyObject): void
 }
 
 declare module 'ins-tools-packaging' {
   export = insToolsPackaging
+}
+
+interface AnyObject {
+  [key: string]: any
+}
+
+interface AxiosInstance {
+  <T = any>(value: T): Promise<T>
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,204 @@
+interface AnyObject {
+  [key: string]: any
+}
+
+interface AxiosInstance {
+  <T = any>(value: T): Promise<T>
+}
+
 export function random(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+/**
+ * [链式取值]
+ *
+ * @param   {Function}   fn            [于函数中返回的取值对象]
+ * @param   {any}  defaultValue  [可选默认返回值]
+ *
+ * @return  {[type]}                   [return description]
+ */
+export function getValue(fn: () => any, defaultValue?: any) {
+  try {
+    const result = fn();
+    const nullish = [null, undefined];
+
+    if (!result && nullish.includes(result)) throw new Error(`get fn() error: ${result}`);
+
+    return result;
+  } catch (error) {
+    // console.warn('get value error:', error)
+    return defaultValue;
+  }
+}
+
+/**
+ * 链式访问器
+ * @param {Any} result  [访问对象]
+ * @param {String} path [访问链地址，例：'data.pageInfo.list.0']
+ * @return {Any}
+ */
+export function chainAccess(result: AnyObject, path: string) {
+  const aPath = path.split(".");
+  let newRes = result[aPath.shift() || ''];
+
+  if (aPath.length && newRes) newRes = chainAccess(newRes, aPath.join("."));
+
+  return newRes;
+}
+
+/**
+ * 防抖
+ *
+ * @return  {[Function]}     [return 防抖加工后的新方法]
+ */
+export function joinDebounce() {
+  let timer: NodeJS.Timeout | string = '';
+
+  return (func: () => AnyObject, ms = 500) => {
+    clearTimeout(timer);
+    timer = setTimeout(func, ms);
+  }
+}
+
+/**
+ * 生成并行异步请求列表
+ *
+ * @param   {[Promise]}  apis  [Promise.all([...])]
+ *
+ * @return  {[Promise]}        [return description]
+ */
+export function genrateParallels(apis: Function[]) {
+  const parallels = apis.map(api => (async () => await api)());
+
+  return parallels;
+}
+
+/**
+ * API then回调处理
+ * @param {Object} res  [resolve参数]
+ */
+export function $thenBack(res: AnyObject) {
+  const data = res?.data;
+  const isError = !data?.success;
+
+  if (isError) throw data;
+
+  return data;
+}
+
+/**
+ * API catch回调处理
+ * @param {String} errPrefix  [自定义错误前缀]
+ */
+export function $catchBack(errPrefix = 'request exception - ') {
+  // @ts-ignore
+  const t = this;
+
+  return function (err: AnyObject) {
+    const [backData, errorMsg] = [
+      { ...err },
+      errPrefix + (err?.msg || err?.errorMsg || ''),
+    ];
+
+    t.$message.error(errorMsg);
+
+    return backData;
+  }
+}
+
+/**
+ * 接口请求封装
+ *
+ * @param   {[Promise]}  api  [接口]
+ *
+ * @return  {[Promise]}       [回调处理后的新接口]
+ */
+export function apiReq(api: AxiosInstance) {
+  // @ts-ignore
+  const { $thenBack, $catchBack } = this;
+
+  // @ts-ignore
+  return (...params: any[]) => api(...params).then($thenBack).catch($catchBack());
+}
+
+/**
+ * 文件下载
+ *
+ * @param   {[any]}  res  接口回调
+ *
+ * @return  {[type]}       [return description]
+ */
+export function $downloadFile(res: AnyObject) {
+  const { data, headers } = res || {};
+  const isError = !data;
+
+  if (isError) throw res;
+
+  const { 'content-disposition': contDesc, 'content-type': contType } = headers || {};
+
+  const type = contType?.split(';').find((v: AnyObject) => v.includes('application'));
+  const fileName = contDesc?.split(';').find((v: AnyObject) => v.includes('filename='))?.replace('filename=', '');
+
+  const decodeName = fileName ? decodeURIComponent(fileName) : '气象导出';
+
+  const [blob, eLink] = [
+    new Blob([data], { type }),
+    document.createElement("a")
+  ];
+
+  eLink.download = decodeName;
+  eLink.style.display = "none";
+  eLink.href = URL.createObjectURL(blob);
+  document.body.appendChild(eLink);
+  eLink.click();
+  URL.revokeObjectURL(eLink.href);
+  document.body.removeChild(eLink);
+}
+
+/**
+ * item字段映射
+ *
+ * @param   {[Object]}  fieldsMap  [传入字段映射表，返回返]
+ *
+ * @return  {[Function]}             [return 映射转换]
+ */
+export function itemFiledsMap(fieldsMap: AnyObject) {
+  return function (item = {}) {
+    const formatItem = {};
+
+    Object.entries(fieldsMap).forEach(([key, path]) => {
+      Object.defineProperty(formatItem, key, {
+        value: chainAccess(item, path),
+        writable: true,
+        enumerable: true,
+        configurable: true
+      })
+      // formatItem[key] = chainAccess(item, path);
+    });
+
+    return formatItem;
+  }
+}
+
+/**
+ * API工具组合
+ *
+ * @var {[type]}
+ */
+export const apiTools = { apiReq, $thenBack, $catchBack, $downloadFile };
+
+
+// /**
+//  * 请求提交定制确认
+//  * @param {String} tip  [确认提示语]
+//  * @param {Function} thenBack  [确认后执行回调]
+//  */
+// export function $confirmReq(tip = '', thenBack = () => null) {
+//   this.$confirm(tip, '提示', {
+//     confirmButtonText: '确定',
+//     cancelButtonText: '取消',
+//   }).then(res => {
+//     setTimeout(() => thenBack(res), 300) // 避免遮罩层关闭时与下一个弹窗开启冲突
+//   }).catch(() => null)
+// }
